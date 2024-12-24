4c325fbd05ed1ac7ddb4e2e0bc3a92f2fced00bf
Suff16 <149042178+Suff16@users.noreply.github.com>
2024-12-25 01:16:16 +0700

    Add files via upload

---
+++ project-sedotmampet/index.js
@@ -0,0 +1,8 @@
+// JavaScript for Mobile Menu Toggle
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
+console.warn();
